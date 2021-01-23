6837a981215a7eb4b8de0af408a6d03ad86cfc93
Randel Ramirez <randelramirez1@gmail.com>
2021-01-23 18:14:18 +0800

    Added infrastructure project

---
+++ Infrastructure/Middleware/ApiExceptionOptions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Middleware
+{
+    public class ApiExceptionOptions
+    {
+        public Action<HttpContext, Exception, ApiError> AddResponseDetails { get; set; }
+    }
+}
