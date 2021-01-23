df7f1b0da652be74c8fe586cdbf1eb973de13f9c
Randel Ramirez <randelramirez1@gmail.com>
2021-01-23 18:15:15 +0800

    Code clean up

--- Infrastructure/Middleware/ApiError.cs
+++ Infrastructure/Middleware/ApiError.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Infrastructure.Middleware
+﻿namespace Infrastructure.Middleware
 {
     public class ApiError
     {
--- Infrastructure/Middleware/ApiExceptionMiddleware.cs
+++ Infrastructure/Middleware/ApiExceptionMiddleware.cs
@@ -2,9 +2,7 @@
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.Net;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Middleware
